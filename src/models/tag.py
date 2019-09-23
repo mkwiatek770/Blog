c5c0853dfb8b3c478fbf62912458c09df3e0e7b9
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-09-23 12:14:03 +0200

    Added unique constraint for name field of TagModel.

--- src/models/tag.py
+++ src/models/tag.py
@@ -5,7 +5,7 @@ class TagModel(db.Model):
     __tablename__ = "blog_tag"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False, unique=True)
 
     @classmethod
     def find_by_id(cls, _id: int) -> "TagModel":
