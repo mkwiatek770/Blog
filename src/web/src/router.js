3befec589693f4b4b811fefc66668d10ad6c539d
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-11-04 13:48:11 +0100

    Initial changes in Vue.js project

--- src/web/src/router.js
+++ src/web/src/router.js
@@ -7,8 +7,7 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home,
@@ -19,7 +18,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: () => import('./views/ToDo.vue')
+    }
   ],
 });
---
+++ src/web/src/views/ToDo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="todo">
+    <h1>Personal Blog TO DO</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
