08ec6c1861dc386c8ed70be738c3ef6e938e1023
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-11-06 14:33:54 +0100

    Improved look of navbar

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Navbar />
+    <Navbar current="Blog" />
     <router-view />
   </div>
 </template>
--- src/web/src/components/Navbar.vue
+++ src/web/src/components/Navbar.vue
@@ -1,10 +1,15 @@
 <template>
   <nav id="nav">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">4Code</a>
+      <div class="hide-on-small-only">
+        <a href="#" class="brand-logo left">4Code</a>
+      </div>
+      <div class="hide-on-med-and-up">
+        <a href="#" class="brand-logo left">4Code | {{ current }}</a>
+      </div>
 
       <div class="brand-logo center">
-        <ul id="nav-mobile" class="hide-on-med-and-down">
+        <ul id="nav-mobile" class="hide-on-small-only">
           <li>
             <a href>Blog</a>
           </li>
@@ -17,29 +22,45 @@
         </ul>
       </div>
 
-      <div class="right">
-        <div class="center row">
-          <div class="col s12">
-            <div class="row" id="topbarsearch">
-              <div class="input-field col s6 s12 red-text">
-                <i class="red-text material-icons prefix">search</i>
-                <input
-                  type="text"
-                  placeholder="search"
-                  id="autocomplete-input"
-                  class="autocomplete red-text"
-                />
+      <div class="hide-on-med-and-down">
+        <div class="right">
+          <div class="center row">
+            <div class="col s12">
+              <div class="row" id="topbarsearch">
+                <div class="input-field col s6 s12 red-text">
+                  <i class="red-text material-icons prefix">search</i>
+                  <input
+                    type="text"
+                    placeholder="search"
+                    id="autocomplete-input"
+                    class="autocomplete red-text"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <ul class="right hide-on-large-only">
+        <li>
+          <a href="sass.html">
+            <i class="material-icons">search</i>
+          </a>
+        </li>
+        <li>
+          <a href="badges.html">
+            <i class="material-icons">menu</i>
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["current"]
+};
 </script>
 
 <style>
